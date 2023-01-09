c12ac9612d7b0929caa0f41a7ba9b5213a8fd995
pathai <pathai.s@linecorp.com>
2023-01-09 20:41:47 +0700

    feat: add use function

--- app/src/main/java/com/anaytist/utils/Application.kt
+++ app/src/main/java/com/anaytist/utils/Application.kt
@@ -8,45 +8,47 @@ import com.appsflyer.AppsFlyerLib
 import com.appsflyer.attribution.AppsFlyerRequestListener
 import java.util.HashMap
 
-fun logEvent(instance: Context) {
+val appsflyer = AppsFlyerLib.getInstance()
+val LOG_TAG = "Ana Log"
 
-    val LOG_TAG = "Ana Log"
+object TDHAnalytics {
+    fun use(instance: Context, afDevKey: String, isDebug: Boolean) {
+        // For debug - remove in production
+        // For debug - remove in production
+        appsflyer.setDebugLog(isDebug)
 
-    val afDevKey = "WF8nbmFZN4DYUQANaCpMsQ"
-    val appsflyer = AppsFlyerLib.getInstance()
-    // For debug - remove in production
-    // For debug - remove in production
-    appsflyer.setDebugLog(true)
+        appsflyer.init(afDevKey, null, instance)
 
-    appsflyer.init(afDevKey, null, instance);
-
-    appsflyer.start(instance, afDevKey, object : AppsFlyerRequestListener {
-        override fun onSuccess() {
-            Log.d(LOG_TAG, "Launch sent successfully woi")
-        }
-
-        override fun onError(errorCode: Int, errorDesc: String) {
-            Log.d(LOG_TAG, "Launch failed to be sent:\n" +
-                    "Error code: " + errorCode + "\n"
-                    + "Error description: " + errorDesc)
-        }
-    });
-
-    val eventValues = HashMap<String, Any>()
-    eventValues.put(AFInAppEventParameterName.PRICE, 1234.56)
-    eventValues.put(AFInAppEventParameterName.CONTENT_ID,"1234567")
-
-    appsflyer.logEvent(instance ,
-        AFInAppEventType.ADD_TO_CART , eventValues, object : AppsFlyerRequestListener {
+        appsflyer.start(instance, afDevKey, object : AppsFlyerRequestListener {
             override fun onSuccess() {
-                Log.d(LOG_TAG, "Event sent successfully wa")
+                Log.d(LOG_TAG, "Launch sent successfully woi")
             }
+
             override fun onError(errorCode: Int, errorDesc: String) {
-                Log.d(LOG_TAG, "Event failed to be sent:\n" +
+                Log.d(LOG_TAG, "Launch failed to be sent:\n" +
                         "Error code: " + errorCode + "\n"
                         + "Error description: " + errorDesc)
             }
-        })
+        });
+    }
 
-    Log.d("Ana Logger", "---- > > > new ver na")
+    fun logEvent(instance: Context) {
+        val eventValues = HashMap<String, Any>()
+        eventValues.put(AFInAppEventParameterName.PRICE, 1234.56)
+        eventValues.put(AFInAppEventParameterName.CONTENT_ID,"1234567")
+
+        appsflyer.logEvent(instance ,
+            AFInAppEventType.ADD_TO_CART , eventValues, object : AppsFlyerRequestListener {
+                override fun onSuccess() {
+                    Log.d(LOG_TAG, "Event sent successfully wa")
+                }
+                override fun onError(errorCode: Int, errorDesc: String) {
+                    Log.d(LOG_TAG, "Event failed to be sent:\n" +
+                            "Error code: " + errorCode + "\n"
+                            + "Error description: " + errorDesc)
+                }
+            })
+
+        Log.d("Ana Logger", "---- > > > new ver na")
+    }
 }
