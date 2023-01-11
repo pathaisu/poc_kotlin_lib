f56341a31e4f16c9621cd7947a6f7d7f11d3b642
pathai <pathai.s@linecorp.com>
2023-01-11 22:50:56 +0700

    feat: refactor to class

--- app/src/main/java/com/tdhanalytics/utils/Application.kt
+++ app/src/main/java/com/tdhanalytics/utils/Application.kt
@@ -8,100 +8,86 @@ import com.appsflyer.attribution.AppsFlyerRequestListener
 import com.google.firebase.analytics.FirebaseAnalytics
 import java.util.HashMap
 
-
-var appsflyer = AppsFlyerLib.getInstance()
 var LOG_TAG = "Ana Log"
-var appFlyerKey = ""
-var tools = ArrayList<String>()
-var instance: Context? = null
-var sessionParameters = HashMap<String, Any>()
+var analyticsApp = ArrayList<AnalyticsProvider>()
 
-var firebaseInstance: FirebaseAnalytics? = null
-
-fun AppsFlyerAnalyticsProvider(context: Context, afDevKey: String, isDebug: Boolean): String {
-    // For debug - remove in production
-    // For debug - remove in production
-    appsflyer.setDebugLog(isDebug)
-
-    appsflyer.init(afDevKey, null, context)
-
-    appFlyerKey = afDevKey
-
-    instance = context
-
-    return "appFlyer"
+interface AnalyticsProvider {
+    fun start()
+    fun logEvent(eventName: String, eventValues: HashMap<String, Any>)
 }
 
-fun FirebaseAnalyticsProvider(firebaseContext: FirebaseAnalytics): String {
-    firebaseInstance = firebaseContext
+class AppsFlyerAnalyticsProviders (context: Context, afDevKey: String, isDebug: Boolean): AnalyticsProvider {
+    var context: Context = context
+    var afDevKey = afDevKey
+    var isDebug = isDebug
+    var appsflyer = AppsFlyerLib.getInstance()
 
-    return "firebase"
+    init {
+        appsflyer.setDebugLog(isDebug)
+        appsflyer.init(afDevKey, null, context)
+    }
+
+    override fun start() {
+        appsflyer.start(context, afDevKey, object : AppsFlyerRequestListener {
+            override fun onSuccess() {
+                Log.d(LOG_TAG, "Launch sent successfully waaa")
+            }
+
+            override fun onError(errorCode: Int, errorDesc: String) {
+                Log.d(LOG_TAG, "Launch failed to be sent:\n" +
+                        "Error code: " + errorCode + "\n"
+                        + "Error description: " + errorDesc)
+            }
+        })
+    }
+
+    override fun logEvent(eventName: String, eventValues: HashMap<String, Any>) {
+        appsflyer.logEvent(
+            context,
+            eventName, eventValues, object : AppsFlyerRequestListener {
+                override fun onSuccess() {
+                    Log.d(LOG_TAG, "$eventName $eventValues")
+                    Log.d(LOG_TAG, "Event sent successfully wa")
+                }
+
+                override fun onError(errorCode: Int, errorDesc: String) {
+                    Log.d(
+                        LOG_TAG, "Event failed to be sent:\n" +
+                                "Error code: " + errorCode + "\n"
+                                + "Error description: " + errorDesc
+                    )
+                }
+            })
+    }
+
+}
+
+class FirebaseAnalyticsProvider(firebaseContext: FirebaseAnalytics): AnalyticsProvider {
+    override fun start() {
+        TODO("Not yet implemented")
+    }
+
+    override fun logEvent(eventName: String, eventValues: HashMap<String, Any>) {
+        TODO("Not yet implemented")
+    }
+
 }
 
 object TDHAnalytics {
-    fun use(tool: String) {
-        Log.d(LOG_TAG, "Platform $tool")
-        tools.add(tool)
-    }
-
-    fun addSessionParameter(params: HashMap<String, Any>) {
-        sessionParameters = params
+    fun use(analytic: AnalyticsProvider) {
+        Log.d(LOG_TAG, "Platform")
+        analyticsApp.add(analytic)
     }
 
     fun start() {
-        if (tools.contains("appFlyer")) {
-            instance?.let {
-                appsflyer.start(it, appFlyerKey, object : AppsFlyerRequestListener {
-                    override fun onSuccess() {
-                        Log.d(LOG_TAG, "Launch sent successfully woi")
-                    }
-
-                    override fun onError(errorCode: Int, errorDesc: String) {
-                        Log.d(LOG_TAG, "Launch failed to be sent:\n" +
-                                "Error code: " + errorCode + "\n"
-                                + "Error description: " + errorDesc)
-                    }
-                })
-            };
+        analyticsApp.forEach {
+            it.start()
         }
     }
 
     fun logEvent(eventName: String, eventValues: HashMap<String, Any>) {
-        // val eventValues = HashMap<String, Any>()
-        // eventValues.put(AFInAppEventParameterName.PRICE, 1234.56)
-        // eventValues.put(AFInAppEventParameterName.CONTENT_ID,"1234567")
-
-        if (tools.contains("appFlyer")) {
-            instance?.let {
-                appsflyer.logEvent(
-                    it,
-                    eventName, eventValues, object : AppsFlyerRequestListener {
-                        override fun onSuccess() {
-                            Log.d(LOG_TAG, "$eventName $eventValues")
-                            Log.d(LOG_TAG, "Event sent successfully wa")
-                        }
-
-                        override fun onError(errorCode: Int, errorDesc: String) {
-                            Log.d(
-                                LOG_TAG, "Event failed to be sent:\n" +
-                                        "Error code: " + errorCode + "\n"
-                                        + "Error description: " + errorDesc
-                            )
-                        }
-                    })
-            }
+        analyticsApp.forEach {
+            it.logEvent(eventName, eventValues)
         }
-
-        if (tools.contains("firebase")) {
-            val bundle = Bundle()
-
-            bundle.putString("show_name", "test value")
-
-            firebaseInstance?.logEvent(FirebaseAnalytics.Event.SELECT_ITEM, bundle)
-
-            Log.d("Ana Logger", "Fireeeeeee baseeeeeee firerrrr")
-        }
-
-        Log.d("Ana Logger", "---- > > > new ver na")
     }
 }
